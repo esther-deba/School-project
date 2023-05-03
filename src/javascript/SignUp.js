e5fa0ca88081dd87a84fb8a84d90dec71ffba596
esther-deba <debaesther72@gmail.com>
2023-05-03 08:32:09 +0100

    SignUP

--- src/javascript/SignUp.js
+++ src/javascript/SignUp.js
@@ -1,49 +1,64 @@
+import { useState } from 'react';
 import '../styling/SignUp.css';
-import { MdKeyboardArrowRight } from 'react-icons/md';
-import { MdOutlineExpandMore } from 'react-icons/md';
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 
 export const SignUp = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setPasswordMatch(false);
+    } else {
+      setPasswordMatch(true);
+    }
+  };
+
   return (
-    <div className='sign-up'>
-      <div className='container'>
-        <div className='todo'>Create a new account</div>
-        <div className='new-user-information'>
-          <div className='left-side'>
-            <div>
-              <div className='to-enter'>User Name :</div>
-              <input type='text' placeholder='ex : Mohamed' />
-            </div>
-            <div>
-              <div className='to-enter'>Email :</div>
-              <input type='text' placeholder='ex : mohamed@gmail.com' />
-            </div>
-          </div>
-          <div className='right-side'>
-            <div>
-              <div className='to-enter'>Town :</div>
-
-              <input type='text' placeholder='Choose where you live' />
-            </div>
-
-            <div>
-              <div className='to-enter'>Date of birth :</div>
-
-              <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-              />
-            </div>
-          </div>
+    <form className='sign-up' onSubmit={handleFormSubmit}>
+      <h1>Create a new account</h1>
+      <div className='new-user-information'>
+        <div>
+          <p>User Name :</p>
+          <input type='text' placeholder='Ex : Mohamed' />
+        </div>
+        <div>
+          <p>Email :</p>
+          <input type='text' placeholder='Ex : mohamed@gmail.com' />
+        </div>
+        <div>
+          <p>Password</p>
+          <input
+            type='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <p>Confirm password</p>
+          <input
+            type='password'
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+          {!passwordMatch && (
+            <p className='passwords-dont-match'>Passwords do not match.</p>
+          )}
+        </div>
+      </div>
+      <div className='container-photo-valide'>
+        <div className='select-photo-container'>
+          <p>No selected photo yet</p>
+          <input type='file' />
         </div>
         <div className='bottom'>
-          <button className='create'>Create</button>
+          <button type='submit' className='create'>
+            Create
+          </button>
           <div>I already have an account</div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
